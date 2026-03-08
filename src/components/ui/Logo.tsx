981393c907c409359e1e7a133d316c89bf88840c
Jonel Hatwell <jonel.hatwell@gmail.com>
2026-03-08 11:25:32 +0800

    fix query (episode resources)

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
         <div className="flex items-center gap-2">
-            <span className="font-bold text-lg hidden md:inline italic">Anime<span className='bg-red-500 text-white px-1 rounded-md ms-1'>Hub</span></span>
+            <span className="font-bold text-lg hidden md:inline italic">Anime<span className='bg-red-500 text-white px-1 rounded-md ms-1'>Stream</span></span>
         </div>
   )
 }
