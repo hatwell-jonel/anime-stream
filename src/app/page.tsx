140b1507bf7dc144791bb1c6dfdfbd6dd3f60cc9
Jonel Hatwell <jonel.hatwell@gmail.com>
2026-03-09 23:13:51 +0800

    remove row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
                                     })}
                               </div>
                         </>
-
+{/* 
                         <>
                               <h2 className="text-2xl font-medium uppercase tracking-wider mb-8">Top Upcoming</h2>
                               <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-5">
@@ -146,7 +146,7 @@ export default function Home() {
                                           );
                                     })}
                               </div>
-                        </>
+                        </> */}
 
                   </section>
             </main>
