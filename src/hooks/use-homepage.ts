7993a0fafd5a97814d0c78a5cbff54317b6c89c0
Jonel Hatwell <jonel.hatwell@gmail.com>
2026-03-09 23:11:22 +0800

    add gc

--- src/hooks/use-homepage.ts
+++ src/hooks/use-homepage.ts
@@ -1,48 +1,38 @@
 'use client';
+
 import { orpc } from '@/lib/tanstackquery/orpc';
 import { useQuery } from '@tanstack/react-query';
 
-const MINUTE =  60 * 1000; // 1 minute in milliseconds
+const MINUTE = 60 * 1000;
 
 function useHomepage() {
-
-    const {data, isLoading} = useQuery({
+    const { data, isLoading } = useQuery({
         ...orpc.anime.getHomePage.queryOptions({}),
-        staleTime: 5 * MINUTE,
+        staleTime: 60 * MINUTE,
+        gcTime: 2 * 60 * MINUTE,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        select: (data) => ({
+            topTenToday: data.top10Animes?.today,
+            topTenWeek: data.top10Animes?.week,
+            topTenMonth: data.top10Animes?.month,
+            mostPopularAnimes: data.mostPopularAnimes,
+            topUpcomingAnimes: data.topUpcomingAnimes,
+            topAiringAnimes: data.topAiringAnimes,
+            spotlightAnimes: data.spotlightAnimes,
+            trendingAnimes: data.trendingAnimes,
+            latestCompletedAnimes: data.latestCompletedAnimes,
+            mostFavoriteAnimes: data.mostFavoriteAnimes,
+        }),
     });
 
-    const topTen = data?.top10Animes;
-
-    const topTenToday = topTen?.today;
-    const topTenWeek = topTen?.week;
-    const topTenMonth = topTen?.month;
-    const mostPopularAnimes = data?.mostPopularAnimes;
-    const topUpcomingAnimes = data?.topUpcomingAnimes;
-    const topAiringAnimes = data?.topAiringAnimes;
-    const spotlightAnimes = data?.spotlightAnimes;
-    const trendingAnimes = data?.trendingAnimes;
-    const latestCompletedAnimes = data?.latestCompletedAnimes;
-    const mostFavoriteAnimes = data?.mostFavoriteAnimes;
-
-
     return {
-        isTop10Loading : isLoading,
-        topTenToday,
-        topTenWeek,
-        topTenMonth,
-        mostPopularAnimes,
-        topUpcomingAnimes,
-        topAiringAnimes,
-        spotlightAnimes,
-        trendingAnimes,
-        latestCompletedAnimes,
-        mostFavoriteAnimes
-    } 
+        isTop10Loading: isLoading,
+        ...data
+    };
 }
 
-export default useHomepage
-
-
+export default useHomepage;
 /**
  * NOTES:
  * staleTime in React Query defines how long the data fetched by a query is considered fresh.
